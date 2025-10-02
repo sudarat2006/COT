35ba10b900e0713ee83247740c4212251432132a
sudarat2006 <sudarat.wanthawee@gmail.com>
2025-10-02 21:15:48 +0700

    Update ALLOWED_HOSTS and fix Vercel configuration; add build script

--- myapp/settings.py
+++ myapp/settings.py
@@ -28,7 +28,7 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 
 
-ALLOWED_HOSTS = ['127.0.0.1','.vercel.app','localhost']
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app','localhost','*']
 
 # Application definition
 INSTALLED_APPS = [
