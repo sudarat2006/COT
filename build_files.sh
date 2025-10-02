82bc80939c258fc60cbeeba90dc7c55f211a6990
sudarat2006 <sudarat.wanthawee@gmail.com>
2025-10-02 21:49:30 +0700

    Restore .gitignore file to exclude common Python artifacts and environment files

--- build_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Installing dependencies..."
-pip install -r requirements.txt
-
-echo "Collecting static files..."
-python3.9 manage.py collectstatic --noinput --clear
-
-echo "Build completed!"
